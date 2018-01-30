ef89dce74221fec7fcb146fd715b656beace5af3
Eudson Bambo <eudson.bambo@ucglobalprograms.org>
2018-01-30 16:44:35 +0200

    Adding the base file for chart default colors

---
+++ static/gla/more/colors.js
@@ -0,0 +1,13 @@
+var colors_bar_chart = ["#26B99A", "#03586A", "#73879C", "#9CC2CB", "#9CA9AE"];
+var color_pie_graph_background = [
+                        "#455C73", "#9B59B6", "#BDC3C7", "#26B99A", "#3498DB",
+                        "#455C73", "#9B59B6", "#BDC3C7", "#26B99A", "#3498DB",
+                        "#455C73", "#9B89B6", "#BDC3C7", "#26B99A", "#3498DB",
+                        "#455C73", "#9B59B6", "#BDC3C7", "#26B99A", "#3498DB"
+                    ];
+var color_pie_chart_hover_background = [
+                        "#34495E", "#B370CF", "#CFD4D8", "#36CAAB", "#49A9EA",
+                        "#34495E", "#B370CF", "#CFD4D8", "#36CAAB", "#49A9EA",
+                        "#34495E", "#B370CF", "#CFD4D8", "#36CAAB", "#49A9EA",
+                        "#34495E", "#B370CF", "#CFD4D8", "#36CAAB", "#49A9EA"
+                    ];
