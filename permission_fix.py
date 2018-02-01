9086fa26a5e7664675194c137026978849f10a2b
Fitti Weissglas <fitti.weissglas@ucsf.edu>
2018-01-31 16:07:04 -0800

    Added a helper to fix view and list permissions, which should be run from within a Python shell.

---
+++ permission_fix.py
@@ -0,0 +1,49 @@
+#from django.db.models.signals import post_syncdb
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import Permission
+
+# TO RUN THIS SCRIPT, OPEN A PYTHON CONSOLE FROM WITHIN PYCHARM
+# THEN TYPE:
+# from xf_system.permission_fix import add_view_permissions
+# add_view_permissions(None)
+# add_list_permissions(None)
+
+
+# make sure to use the right settings file so that you can do this on the correct db
+
+
+def add_view_permissions(sender, **kwargs):
+    """
+    This syncdb hooks takes care of adding a view permission too all our
+    content types.
+    """
+    # for each of our content types
+    for content_type in ContentType.objects.all():
+        # build our permission slug
+        codename = "view_%s" % content_type.model
+
+        # if it doesn't exist..
+        if not Permission.objects.filter(content_type=content_type, codename=codename):
+            # add it
+            Permission.objects.create(content_type=content_type,
+                                      codename=codename,
+                                      name="Can view %s" % content_type.name)
+            print("Added view permission for %s" % content_type.name)
+
+def add_list_permissions(sender, **kwargs):
+    """
+    This syncdb hooks takes care of adding a view permission too all our
+    content types.
+    """
+    # for each of our content types
+    for content_type in ContentType.objects.all():
+        # build our permission slug
+        codename = "list_%s" % content_type.model
+
+        # if it doesn't exist..
+        if not Permission.objects.filter(content_type=content_type, codename=codename):
+            # add it
+            Permission.objects.create(content_type=content_type,
+                                      codename=codename,
+                                      name="Can list %s" % content_type.name)
+            print("Added list permission for %s" % content_type.name)
