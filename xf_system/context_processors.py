2ac67c4750d0b73d2e18e444542e9221e0d934e1
Fitti Weissglas <fitti.weissglas@ucsf.edu>
2018-04-19 18:41:51 -0700

    Perspectives and User menu now shown on all pages
    * User menu and perspectives now shown on all pages, including CRUD apps

---
+++ xf_system/context_processors.py
@@ -0,0 +1,18 @@
+from xf.uc_dashboards.models import Perspective
+
+
+def include_user_menu(request):
+
+    if request.user.is_authenticated():
+
+        if "perspective_id" in request.session:
+            perspective = Perspective.objects.get(pk=request.session["perspective_id"])
+        else:
+            perspective = None
+
+        request.user.load_perspectives()
+        perspectives = request.user.perspectives
+        return {'menu': "myMenu", 'perspectives':perspectives, 'current_perspective':perspective}
+    else:
+        return {}
+
