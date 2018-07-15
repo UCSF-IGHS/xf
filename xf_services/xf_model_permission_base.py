499f101c1aa482712b8566b682dc48cb8804f710
Fitti Weissglas <fitti.weissglas@ucsf.edu>
2018-07-15 15:18:59 -0700

    Support for role-based security through a security layer

--- xf_services/xf_model_permission_base.py
+++ xf_services/xf_model_permission_base.py
@@ -1,24 +1,53 @@
 
+from xf.xf_services.xf_business_rules_checks import XFBusinessRulesChecks
+
+
 class XFModelPermissionBase:
     model = None
-    security_service = None
+    security_service: XFBusinessRulesChecks = XFBusinessRulesChecks()
 
     @classmethod
-    def can_do_new(cls, user):
-        cls.security_service.ensure_user_has_model_permission(cls.model, user, 'add')
+    def can_do_new(cls, action, user, **kwargs):
+        model_list = kwargs['model_list']
+        cls.security_service.ensure_user_has_model_permission(model_list.model, user, 'add')
+        return True
+
 
     @classmethod
-    def can_do_edit(cls, user, physical_exam):
-        cls.security_service.ensure_user_has_model_permission(cls.model, user, 'change')
+    def can_do_edit(cls, action, user, **kwargs):
+        model_list = kwargs['model_list']
+        cls.security_service.ensure_user_has_model_permission(model_list.model, user, 'change')
+        return True
 
     @classmethod
-    def can_do_delete(cls, user, physical_exam):
-        cls.security_service.ensure_user_has_model_permission(cls.model, user, 'delete')
+    def can_do_edit_instance(cls, action, user, model_instance, **kwargs):
+        cls.security_service.ensure_user_has_model_permission(type(model_instance), user, 'change')
+        return True
 
     @classmethod
-    def can_do_view_details(cls, user, physical_exam):
-        cls.security_service.ensure_user_has_model_permission(cls.model, user, 'view')
+    def can_do_delete(cls, action, user, **kwargs):
+        model_list = kwargs['model_list']
+        cls.security_service.ensure_user_has_model_permission(model_list.model, user, 'delete')
+        return True
 
     @classmethod
-    def can_do_list(cls, user, physical_exam):
-        cls.security_service.ensure_user_has_model_permission(cls.model, user, 'list')
+    def can_do_delete_instance(cls, action, user, model_instance, **kwargs):
+        cls.security_service.ensure_user_has_model_permission(type(model_instance), user, 'delete')
+        return True
+
+    @classmethod
+    def can_do_details(cls, action, user, **kwargs):
+        model_list = kwargs['model_list']
+        cls.security_service.ensure_user_has_model_permission(model_list.model, user, 'view')
+        return True
+
+    @classmethod
+    def can_do_details_instance(cls, action, user, model_instance, **kwargs):
+        cls.security_service.ensure_user_has_model_permission(type(model_instance), user, 'view')
+        return True
+
+    @classmethod
+    def can_do_list(cls, action, user, **kwargs):
+        model_list = kwargs['model_list']
+        cls.security_service.ensure_user_has_model_permission(model_list.model, user, 'list')
+        return True
