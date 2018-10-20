291d8bdaf48d5a9829502a3ce1da568f9a4ff827
David Mugume <mugumedavid@gmail.com>
2018-10-19 19:30:11 -0500

    Auto complete dropdown view to process api requests

---
+++ xf_crud/views/auto_complete_view.py
@@ -0,0 +1,54 @@
+from dal import autocomplete
+from django.apps import apps
+from django.db.models import QuerySet, Model
+
+
+class XFAutoCompleteView(autocomplete.Select2QuerySetView):
+
+    def _process_forwarded_parameters(self):
+        self._set_model_from_model_name()
+        self._set_model_field_name()
+
+    def get_queryset(self) -> QuerySet:
+
+        self._process_forwarded_parameters()
+
+        if self.model is None:
+            return Model.objects.none()
+
+        if not self.request.user.is_authenticated:
+            return self.model.objects.none()
+
+        queryset = self.model.objects.all()
+
+        if self.q:
+            kwargs = {
+                '{0}__{1}'.format(self.model_field_name, 'istartswith'): self.q,
+            }
+            queryset = queryset.filter(**kwargs)
+
+        return queryset
+
+    def _set_model_from_model_name(self):
+
+        model_name = None
+
+        for key, value in self.forwarded.items():
+            if key.endswith('_model'):
+                model_name = value
+                self.forwarded.pop(key)
+                break
+
+        if model_name is not None:
+            app_label = model_name.split(".")[0]
+            model_name = model_name.split(".")[1]
+
+            self.model = apps.get_model(app_label=app_label, model_name=model_name)
+
+    def _set_model_field_name(self):
+
+        for key, value in self.forwarded.items():
+            if key.endswith('_search_field'):
+                self.model_field_name = value
+                self.forwarded.pop(key)
+                break
