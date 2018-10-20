ee88efa2f95b2619abc50600d437c3c90d2ed851
David Mugume <mugumedavid@gmail.com>
2018-10-19 21:49:52 -0500

    Unit tests for auto complete view

---
+++ tests/views_tests/auto_complete_view_tests.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.models import User
+from django.test import RequestFactory
+
+from xf.tests.permission_tests.permissions_tests import XFPermissionsTestCase
+from xf.xf_crud.views.auto_complete_view import XFAutoCompleteView
+
+
+class AutoCompleteViewTestCase(XFPermissionsTestCase):
+
+    def test_process_forwarded_parameters(self):
+
+        view = XFAutoCompleteView()
+        view.forwarded = {
+            "_user_model": "auth.User",
+            "_user_search_field": "username"
+        }
+
+        view._process_forwarded_parameters()
+
+        self.assertEqual(view.model, User, 'Expected model to be User')
+        self.assertEqual(view.model_field_name, 'username', "Expected the search model_field_name to be 'username'")
+
+
+    def test_get_queryset(self):
+
+        test_data = self._generate_test_data()
+
+        view = XFAutoCompleteView()
+        view.forwarded = {
+            "_user_model": "auth.User",
+            "_user_search_field": "username"
+        }
+
+        request = RequestFactory().get('/')
+        request.user = test_data['user']
+        view.request = request
+        view.q = 'test'
+
+        queryset = view.get_queryset()
+        self.assertEqual(queryset.count(), 1, "Expecting one user with username starting with 'test'")
+        self.assertEqual(queryset.first().username, 'test', "Expecting one user with username 'test'")
+
